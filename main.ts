8757405a29e66ce758de483759ce18a1fbb6aaf8
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-29 17:57:00 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,3 +12,22 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
 radio.comment(input.temperature())
 sender.beimStart(storage.getNumber(StorageSlots.s1))
 storage.putNumber(StorageSlots.s1, sender.storageBufferGet())
+loops.everyInterval(400, function () {
+    if (sender.isFunktion(sender.eFunktion.ng) && sender.joystickQwiic()) {
+        basic.setLedColor(0x00ff00)
+        radio.fill_sendBuffer19()
+        if (sender.isFunktion(sender.eFunktion.m0_s0)) {
+            sender.sendM0(radio.radio_sendBuffer19())
+        } else if (sender.isFunktion(sender.eFunktion.m0_m1_s0)) {
+            sender.sendM01(radio.radio_sendBuffer19())
+        } else if (sender.isFunktion(sender.eFunktion.ma_mb)) {
+            sender.sendMAB(radio.radio_sendBuffer19())
+        } else if (sender.isFunktion(sender.eFunktion.mc_mb)) {
+            sender.sendMCB(radio.radio_sendBuffer19())
+        }
+        radio.sendData(radio.radio_sendBuffer19())
+        radio.zeige5x5Buffer(radio.radio_sendBuffer19())
+        radio.zeige5x5Joystick(radio.radio_sendBuffer19())
+        basic.turnRgbLedOff()
+    }
+})
