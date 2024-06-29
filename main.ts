69b7e3654da9d94defeac2bbb4c0928e965003f4
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-29 14:19:10 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     sender.buttonB()
     storage.putNumber(StorageSlots.s1, sender.storageBufferGet())
-    basic.showNumber(sender.storageBufferGet())
 })
 radio.comment(input.temperature())
-basic.showNumber(storage.getNumber(StorageSlots.s1))
-sender.startAuswahl(storage.getNumber(StorageSlots.s1))
+sender.beimStart(storage.getNumber(StorageSlots.s1))
 storage.putNumber(StorageSlots.s1, sender.storageBufferGet())
