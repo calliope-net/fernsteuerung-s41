9beb7ce6ea167e7e39a70975000dd4d4e9c74379
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-11 13:59:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+function modell_Callibot () {
+    if (sender.isFunktion(sender.eFunktion.m0_s0)) {
+        sender.sendM0(radio.radio_sendBuffer19())
+    } else if (sender.isFunktion(sender.eFunktion.mc_md_callibot_beispiele)) {
+        radio.setBetriebsart(radio.radio_sendBuffer19(), radio.e0Betriebsart.p0)
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor, radio.speedPicker(100))
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.md, radio.eBufferOffset.b0_Motor, radio.speedPicker(50))
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.md, radio.eBufferOffset.b1_Servo, sender.getButtonAB_Counter())
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.md, true)
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.mc, sender.getButtonAB_Switch(sender.eButtonAB_Switch.B))
+    }
+    radio.setSchalter(radio.radio_sendBuffer19(), radio.e0Schalter.b0, sender.joystickButtonPosition())
+}
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (sender.buttonB()) {
         storage.putNumber(StorageSlots.s1, sender.storageBufferGet())
@@ -48,7 +61,7 @@ loops.everyInterval(400, function () {
         radio.comment(sender.multiswitchGrove())
         radio.fill_sendBuffer19()
         if (sender.isModell(sender.eModell.cb2e)) {
-        	
+            modell_Callibot()
         } else if (sender.isModell(sender.eModell.mkcg) || sender.isModell(sender.eModell.mkck)) {
             modell_MKC()
         }
